105c8036523a378cdc351d4f0f675f142e9f6965
Gustavo Ferreira <gustavogardel@gmail.com>
2016-06-14 17:55:49 -0700

    created logic conditionals memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+if (cardTwo === cardFour) {
+	alert("You found a Match!");
+} else {
+	alert("Sorry, try again.");
+}

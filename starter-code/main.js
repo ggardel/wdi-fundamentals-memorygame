8c4afc597e6623b09012fbff465ef510b2072839
Gustavo Ferreira <gustavogardel@gmai.com>
2016-06-17 16:30:01 -0700

    final js memorygame

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,54 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-
-if (cardTwo === cardFour) {
-	alert("You found a Match!");
-} else {
-	alert("Sorry, try again.");
-}
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 
-function createBoard(){
-  for (var i=0; i<cards.length; i++){
+function createBoard() {
 
+  for (var i=0; i<cards.length; i++) {
 
-  var cardElement = document.createElement('div');
-  cardElement.className = 'card';
+    var cardElement = document.createElement('div');
 
-  board.appendChild(cardElement);	
+    cardElement.className = 'card';
+
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
   }
 }
+
+function isTwoCards() {
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  console.log(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = "<img src='Spades 13.png' alt='k of spades'>";
+  } else {
+    this.innerHTML = "<img src='Hearts 12.png' alt='Q of hearts'>";
+  }
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
+
